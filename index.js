4234ff064604f1c15c27a18036c8edc10a831d1c
spaceC00kie <kirstie317@gmail.com>
2021-03-04 01:25:45 -0500

    completed

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
 const PORT = 8080
 
 app.listen(PORT, () => console.log("This dude is bullshittin"))
 
+app.use(express.json())
+
 app.get("/tshirt", (req, res) => {
   res.status(200).send({
-    tshirt: "withSleeves",
+    tshirt: "shirt",
     size: "medium",
+  })
+})
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params
+  const { logo } = req.body
+
+  if (!logo) {
+    res.status(418).send({ message: "More input needed" })
+  }
+
+  res.send({
+    tshirt: `shirt with your ${logo} and ID of ${id}`,
   })
 })
